b44a3ed5c8d7e4bf8767e226c174e0a074585588
Rudraksh Agarwal <rudrakshiitd@outlook.com>
2024-11-10 21:35:22 -0500

    feat: copy nnpredict, nnobjfunction

--- Assignment2/basecode/facennScript.py
+++ Assignment2/basecode/facennScript.py
@@ -31,9 +31,96 @@ def sigmoid(z):
     
 # Replace this with your nnObjFunction implementation
 def nnObjFunction(params, *args):
+    """% nnObjFunction computes the value of objective function (negative log 
+    %   likelihood error function with regularization) given the parameters 
+    %   of Neural Networks, thetraining data, their corresponding training 
+    %   labels and lambda - regularization hyper-parameter.
+
+    % Input:
+    % params: vector of weights of 2 matrices w1 (weights of connections from
+    %     input layer to hidden layer) and w2 (weights of connections from
+    %     hidden layer to output layer) where all of the weights are contained
+    %     in a single vector.
+    % n_input: number of node in input layer (not include the bias node)
+    % n_hidden: number of node in hidden layer (not include the bias node)
+    % n_class: number of node in output layer (number of classes in
+    %     classification problem
+    % training_data: matrix of training data. Each row of this matrix
+    %     represents the feature vector of a particular image
+    % training_label: the vector of truth label of training images. Each entry
+    %     in the vector represents the truth label of its corresponding image.
+    % lambda: regularization hyper-parameter. This value is used for fixing the
+    %     overfitting problem.
+       
+    % Output: 
+    % obj_val: a scalar value representing value of error function
+    % obj_grad: a SINGLE vector of gradient value of error function
+    % NOTE: how to compute obj_grad
+    % Use backpropagation algorithm to compute the gradient of error function
+    % for each weights in weight matrices.
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % reshape 'params' vector into 2 matrices of weight w1 and w2
+    % w1: matrix of weights of connections from input layer to hidden layers.
+    %     w1(i, j) represents the weight of connection from unit j in input 
+    %     layer to unit i in hidden layer.
+    % w2: matrix of weights of connections from hidden layer to output layers.
+    %     w2(i, j) represents the weight of connection from unit j in hidden 
+    %     layer to unit i in output layer."""
+
+    n_input, n_hidden, n_class, training_data, training_label, lambdaval = args
+
+    w1 = params[0:n_hidden * (n_input + 1)].reshape((n_hidden, (n_input + 1)))
+    w2 = params[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
+    obj_val = 0
+
+    n = training_data.shape[0]
+    training_data = np.append(training_data, np.ones((n, 1)), axis=1)
+    h1 = sigmoid(np.dot(training_data, w1.T))
+    h1 = np.append(h1, np.ones((h1.shape[0], 1)), axis=1)
+    output = sigmoid(np.dot(h1, w2.T))
+
+    y = np.zeros((n, n_class))
+    
+    for i, value in enumerate(training_label):
+        digit = int(value)
+        y[i][digit] = 1
+
+    obj_val = -np.sum(y * np.log(output) + (1 - y) * np.log(1 - output)) / n + (lambdaval / (2 * n)) * (np.sum(w1[:, :-1] ** 2) + np.sum(w2[:, :-1] ** 2))
+
+    delta_output = output - y
+    delta_h1 = np.dot(delta_output, w2) * h1 * (1 - h1)
+    delta_h1 = delta_h1[:, :-1]
+
+    grad_w1 = (np.dot(delta_h1.T, training_data) + lambdaval * np.append(w1[:, :-1], np.zeros((n_hidden, 1)), axis=1)) / n
+    grad_w2 = (np.dot(delta_output.T, h1) + lambdaval * np.append(w2[:, :-1], np.zeros((n_class, 1)), axis=1)) / n
+    obj_grad = np.concatenate((grad_w1.flatten(), grad_w2.flatten()), axis=0)
+    return (obj_val, obj_grad)
     
 # Replace this with your nnPredict implementation
 def nnPredict(w1,w2,data):
+    """% nnPredict predicts the label of data given the parameter w1, w2 of Neural
+    % Network.
+
+    % Input:
+    % w1: matrix of weights of connections from input layer to hidden layers.
+    %     w1(i, j) represents the weight of connection from unit i in input 
+    %     layer to unit j in hidden layer.
+    % w2: matrix of weights of connections from hidden layer to output layers.
+    %     w2(i, j) represents the weight of connection from unit i in input 
+    %     layer to unit j in hidden layer.
+    % data: matrix of data. Each row of this matrix represents the feature 
+    %       vector of a particular image
+       
+    % Output: 
+    % label: a column vector of predicted labels"""
+
+    data_bias = np.append(data, np.ones((data.shape[0], 1)), axis=1)
+    h1 = sigmoid(np.dot(data_bias, w1.T))
+    h1 = np.append(h1, np.ones((h1.shape[0], 1)), axis=1)
+    output = sigmoid(np.dot(h1, w2.T))
+    labels = np.argmax(output, axis=1)
+    return labels.reshape((-1,1))
 
 # Do not change this
 def preprocess():
